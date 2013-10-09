eee4bb2bde5877b7777ddd379c54aee94995d4cf
Kevin Qiu <kevin@freshbooks.com>
2013-10-09 16:43:20 -0400

    Leave integration test coverage out for now

--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,18 @@
 from fabric.api import local
 
 
-def _run_coverage(test_subfolder):
-    test_folder = 'test/%s' % test_subfolder
-    local('coverage run --source=btsync,%s '
-          '$(which nosetests) %s' % (test_folder, test_folder))
-
-
 def test():
     """Run unit tests"""
-    _run_coverage('unit')
+    local('coverage run --source=btsync,test/unit '
+          '$(which nosetests) test/unit')
 
 
-def coverage(test_category='unit'):
+def coverage():
     """Run tests and show coverage report"""
-    assert test_category in ('unit', 'integration')
-    if test_category == 'unit':
-        test()
-    else:
-        test_integration()
-
+    test()
     local('coverage report -m')
 
 
 def test_integration():
     """Run integration tests"""
-    _run_coverage('integration')
+    local('nosetests test/integration')
