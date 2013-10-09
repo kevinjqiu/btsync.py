2d1cd387107096789e2235c122f203542aefe4dd
Kevin Qiu <kevin@freshbooks.com>
2013-10-08 23:23:39 -0400

    Create exception class

--- btsync/client.py
+++ btsync/client.py
@@ -22,6 +22,16 @@ def _make_action_method(action_name, key=None):
     return getter
 
 
+class BtsyncException(StandardError):
+    def __init__(self, error):
+        self.error = error
+        self.code = error['error']
+        self.message = error['message']
+
+    def __str__(self):
+        return self.message
+
+
 class Client(object):
     def __init__(self, **kwargs):
         self._host = kwargs.pop('host', '127.0.0.1')
@@ -85,14 +95,19 @@ class Client(object):
     generate_secret = _make_action_method('generatesecret')
 
     def add_sync_folder(self, name, secret, force=False):
-        raise NotImplementedError
+        response = self._make_request(
+            params={'action': 'addsyncfolder',
+                    'name': name,
+                    'secret': secret}
+        )
+        result = json.loads(response.text)
+        if result['error']:
+            raise BtsyncException(result)
 
     def remove_sync_folder(self, name, secret):
         raise NotImplementedError
 
-    @property
-    def settings(self):
-        raise NotImplementedError
+    settings = property(_make_action_method('getsettings', key='settings'))
 
     def set_settings(self, a, b):
         raise NotImplementedError
@@ -111,12 +126,6 @@ class Client(object):
 
     def remove_host(self, name, secret, index):
         raise NotImplementedError
-
-    # def get_lang(self):
-    #     raise NotImplementedError
-
-    # def set_lang(self):
-    #     raise NotImplementedError
 
     def update_secret(self, name, secret, new_secret):
         raise NotImplementedError
