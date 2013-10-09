a19c183c36bd37a6e71167ced3e236217c434f13
Kevin Qiu <kevin@freshbooks.com>
2013-10-09 11:50:12 -0400

    Update fabfile to point to the new location for unit tests

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import local
 
 
 def test():
-    local('coverage run --source=btsync,test $(which nosetests)')
+    local('coverage run --source=btsync,test $(which nosetests) test/unit')
 
 
 def coverage():
