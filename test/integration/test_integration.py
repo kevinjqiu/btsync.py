4d11705ba144fb06b28b83464d3c8472e1a095ca
Kevin Qiu <kevin@freshbooks.com>
2013-10-09 12:59:47 -0400

    Started integration tests

---
+++ test/integration/test_integration.py
@@ -0,0 +1,52 @@
+import json
+import subprocess
+import tempfile
+
+from nose.tools import eq_
+
+import btsync
+
+
+PORT = 37369
+CONFIG = {
+    "device_name": "MY TESTING NODE",
+    "listening_port": 0,
+    "webui":
+    {
+        "listen": "0.0.0.0:%s" % PORT,
+        "login": "admin",
+        "password": "password"
+    }
+}
+
+
+class TestIntegration(object):
+    def setup(self):
+        _, config_file_name = tempfile.mkstemp()
+
+        with open(config_file_name, 'w') as f:
+            json.dump(CONFIG, f)
+
+        self.btsync_process = subprocess.Popen([
+            '/home/kevin/btsync/btsync', '--nodaemon',
+            '--config', config_file_name,
+        ])
+
+        self.client = btsync.Client(
+            host='127.0.0.1',
+            port=PORT,
+            username='admin',
+            password='password',
+        )
+
+    def teardown(self):
+        self.btsync_process.kill()
+
+    def test_add_sync_folder_readonly(self):
+        readonly_secret = self.client.generate_secret()['rosecret']
+        folder_name = tempfile.mkdtemp()
+        self.client.add_sync_folder(folder_name, readonly_secret)
+        sync_folders = self.client.sync_folders
+        eq_(1, len(sync_folders))
+        eq_(folder_name, sync_folders[0]['name'])
+        eq_(readonly_secret, sync_folders[0]['secret'])
