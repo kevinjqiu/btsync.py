e55d62356f9899451df00a13e3bb28f998988dfa
Kevin Qiu <kevin@freshbooks.com>
2013-10-09 16:41:59 -0400

    Randomize ports in integration tests

--- test/integration/test_integration.py
+++ test/integration/test_integration.py
@@ -1,3 +1,4 @@
+import random
 import json
 import subprocess
 import tempfile
@@ -7,14 +8,13 @@ from nose.tools import eq_
 import btsync
 
 
-PORT = 37369
 CONFIG = {
     "device_name": "MY TESTING NODE",
     "listening_port": 0,
     "storage_path": "",
     "webui":
     {
-        "listen": "0.0.0.0:%s" % PORT,
+        "listen": "0.0.0.0:8888",
         "login": "admin",
         "password": "password"
     }
@@ -24,10 +24,13 @@ CONFIG = {
 class TestIntegration(object):
     def setup(self):
         storage_path = tempfile.mkdtemp()
+
         _, config_file_name = tempfile.mkstemp()
 
+        port = random.randint(10000, 65535)
         with open(config_file_name, 'w') as f:
             CONFIG['storage_path'] = storage_path
+            CONFIG['webui']['listen'] = '0.0.0.0:%s' % port
             json.dump(CONFIG, f)
 
         self.btsync_process = subprocess.Popen([
@@ -37,7 +40,7 @@ class TestIntegration(object):
 
         self.client = btsync.Client(
             host='127.0.0.1',
-            port=PORT,
+            port=port,
             username='admin',
             password='password',
         )
@@ -53,3 +56,14 @@ class TestIntegration(object):
         eq_(1, len(sync_folders))
         eq_(folder_name, sync_folders[0]['name'])
         eq_(readonly_secret, sync_folders[0]['secret'])
+        eq_(0, sync_folders[0]['iswritable'])
+
+    def test_add_sync_folder_readwrite(self):
+        secret = self.client.generate_secret()['secret']
+        folder_name = tempfile.mkdtemp()
+        self.client.add_sync_folder(folder_name, secret)
+        sync_folders = self.client.sync_folders
+        eq_(1, len(sync_folders))
+        eq_(folder_name, sync_folders[0]['name'])
+        eq_(secret, sync_folders[0]['secret'])
+        eq_(1, sync_folders[0]['iswritable'])
