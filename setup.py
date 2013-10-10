d2f7da85f69633c9b490b585348e50aa63bfc86a
Kevin Qiu <kevin@freshbooks.com>
2013-10-09 20:38:34 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.9.3'
+version = '0.9.4'
 
 setup(name='btsync.py',
       version=version,
@@ -11,9 +11,10 @@ setup(name='btsync.py',
       keywords='api',
       author='Kevin Jing Qiu',
       author_email='kevin.jing.qiu@gmail.com',
-      url='',
+      url='https://github.com/kevinjqiu/btsync.py',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      data_files=[('.', ['requirements.txt', 'README.md', 'LICENSE'])],
       include_package_data=True,
       zip_safe=False,
       install_requires=open('requirements.txt').readlines(),
